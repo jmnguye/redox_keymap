c3217c2d2b39d08dc5d83b9a8f268b5596bf2155
mxp <maxplayer@free.fr>
2022-07-07 23:48:35 +0200

    updating number of led

--- config.h
+++ config.h
@@ -29,7 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #undef RGBLED_NUM
 #define RGBLIGHT_SLEEP
 #define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 24
+#define RGBLED_NUM 34
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
