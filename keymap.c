1230df818db63eea76c88120d398dd34a52c6dd8
mxp <maxplayer@free.fr>
2023-04-03 22:09:15 +0200

    update blackslash pipe, quote , dble, equal and plus

--- keymap.c
+++ keymap.c
@@ -85,9 +85,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,KC_LBRC ,                          KC_RBRC ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_ENT  ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_HOME ,                          KC_END  ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT ,
+     KC_LSFT ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_PEQL ,                          KC_PPLS ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_PGDN ,KC_PGUP ,        KC_VOLD ,KC_VOLU ,KC_N    ,KC_M    ,KC_COMMA,KC_DOT  ,KC_MINS ,KC_SLASH,
+     KC_LCTL ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_BSLS ,KC_PIPE ,        QUOTE   ,DBQUOTE ,KC_N    ,KC_M    ,KC_COMMA,KC_DOT  ,KC_MINS ,KC_SLASH,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
   LT(1,KC_NO),KC_LCTL ,KC_LGUI ,KC_LALT ,     LT(2,KC_NO) ,KC_BSPC ,KC_DEL  ,         KC_INS ,KC_SPC      ,LT(3,KC_NO)  ,KC_DEL  ,KC_PSCR ,KC_PAUSE,KC_RGUI 
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
