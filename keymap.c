4a6f2b98addd1d5ea33d12e97b7ab9d3d0c4726c
mxp <maxplayer@free.fr>
2023-08-15 17:03:24 +0200

    ajout de la touche espace en mode tape, et egalement de maj

--- keymap.c
+++ keymap.c
@@ -87,11 +87,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,KC_LBRC ,                          KC_RBRC ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_ENT  ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_EQL  ,                          KC_PPLS ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT ,
+     KC_LSFT ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_EQL  ,                          KC_PPLS ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,RSFT_T(KC_QUOT),
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LCTL ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_BSLS ,KC_PIPE ,        DBQUOTE ,QUOTE   ,KC_N    ,KC_M    ,KC_COMMA,KC_DOT  ,KC_MINS ,KC_SLASH,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-  LT(1,KC_PAUSE),KC_LCTL ,KC_LGUI ,KC_LALT ,LT(2,KC_NO),KC_BSPC,LT(4,KC_SLASH),     LT(5,KC_MINS),KC_SPC   ,LT(3,KC_DOT)  ,KC_DEL  ,KC_PSCR ,KC_INS,KC_RSHIFT
+  LT(1,KC_PAUSE),KC_LCTL ,KC_LGUI ,KC_LALT ,LT(2,KC_ESC),KC_BSPC,LT(4,KC_SLASH),     LT(5,KC_MINS),KC_SPC   ,LT(3,KC_DOT)  ,KC_DEL  ,KC_PSCR ,KC_INS,KC_RSHIFT
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
